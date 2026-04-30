e5ea1bbdcf852192ff8d86525450bcc741841942
Obelisk <anusorn.b@gmail.com>
2026-04-30 15:33:36 +0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -3,8 +3,9 @@ from PySide6.QtWidgets import QApplication, QLabel
 
 app = QApplication(sys.argv)
 
-label = QLabel("Hello World")
+label = QLabel("<font color=red size=40>Hello World</font>")
 label.resize(640, 480)
+label.setWindowTitle("My Programe")
 label.show()
 
 sys.exit(app.exec())
